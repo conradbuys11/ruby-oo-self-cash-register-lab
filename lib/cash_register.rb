8ac687fae4953f08ac705c200333af754ecef81c
conrad_buys <conradbuys11@gmail.com>
2020-08-26 13:00:39 -0400

    some changes

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
-#test
+require 'pry'
+
+class CashRegister
+
+    def initialize(discount = 20)
+        @total = 0
+        @discount = discount
+    end
+
+    attr_accessor :total
+    attr_accessor :discount
+
+    def add_item(item, price, quantity = 1)
+        self.total += price * quantity
+    end
+
+    def apply_discount()
+        self.total -= self.total * (self.discount / 100.0) #reduce our price by the price times the discount
+        return "After the discount, the total comes to $#{self.total.to_i}."
+    end
+
+end
